9c3e2f4d2fa7e59a99c7a602bf2f2a50a57a988e
Ashay Jaiswal <ashaywin@gmail.com>
2022-03-14 22:49:34 +0530

    Update start game slide left close event

--- layout.js
+++ layout.js
@@ -3,4 +3,7 @@ const startBut = document.querySelector('#startBut');
 
 startBut.addEventListener('click', () => {
     container.style.animation = "remove 1s";
+    setTimeout(() => {
+        container.style.display = "none";
+    }, 1000)
 });
